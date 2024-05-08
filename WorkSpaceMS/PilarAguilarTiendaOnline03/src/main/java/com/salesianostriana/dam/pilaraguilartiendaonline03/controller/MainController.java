d80ef94464ddb069e67eb139c1b7c0248159bf85
ZaidP6 <pilarads06@gmail.com>
2024-05-08 21:09:33 +0200

    otro dia mas intentando que thymeleaf me deje hacer cosas

--- WorkSpaceMS/PilarAguilarTiendaOnline03/src/main/java/com/salesianostriana/dam/pilaraguilartiendaonline03/controller/MainController.java
+++ WorkSpaceMS/PilarAguilarTiendaOnline03/src/main/java/com/salesianostriana/dam/pilaraguilartiendaonline03/controller/MainController.java
@@ -6,9 +6,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MainController {
 
-	@GetMapping("/")
+	/*
+	 * @GetMapping("/")
 	public String index() {
 		return "index";
 	}
+	 */
 	
 }
