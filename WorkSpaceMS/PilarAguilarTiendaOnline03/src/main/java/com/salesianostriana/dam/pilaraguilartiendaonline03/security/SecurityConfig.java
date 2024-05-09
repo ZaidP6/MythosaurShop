b36e842a409f2e0457f77802229c56109225aa6d
ZaidP6 <pilarads06@gmail.com>
2024-05-09 20:13:15 +0200

    securityConfig creada logIn en index, falta modificar

--- WorkSpaceMS/PilarAguilarTiendaOnline03/src/main/java/com/salesianostriana/dam/pilaraguilartiendaonline03/security/SecurityConfig.java
+++ WorkSpaceMS/PilarAguilarTiendaOnline03/src/main/java/com/salesianostriana/dam/pilaraguilartiendaonline03/security/SecurityConfig.java
@@ -1,4 +1,66 @@
 package com.salesianostriana.dam.pilaraguilartiendaonline03.security;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+
+    @Bean
+    AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
+
+        AuthenticationManagerBuilder authBuilder = http.getSharedObject(AuthenticationManagerBuilder.class);
+
+        return authBuilder.authenticationProvider(daoAuthenticationProvider()).build();
+
+    }
+
+    @Bean
+    InMemoryUserDetailsManager userDetailsService() {
+        UserDetails user = User.builder()
+                .username("admin").password("{noop}admin")
+                .roles("ADMIN").build();
+        return new InMemoryUserDetailsManager(user);
+    }
+
+    @Bean
+    DaoAuthenticationProvider daoAuthenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setUserDetailsService(userDetailsService());
+        provider.setPasswordEncoder(PasswordEncoderFactories.createDelegatingPasswordEncoder());
+        return provider;
+    }
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        /*
+         * http .authorizeRequests()
+         * .antMatchers("/css/**","/js/**","/webjars/**").permitAll()
+         * .anyRequest().authenticated() .and() .formLogin() .loginPage("/login")
+         * .permitAll();
+         *
+         * return http.build();
+         */
+
+        http.authorizeHttpRequests(
+                        (authz) -> authz
+                                .requestMatchers("/css/**", "/js/**").permitAll()
+                                .anyRequest().authenticated())
+                .formLogin((loginz) -> loginz
+                        .loginPage("/form/logIn").permitAll());
+
+        return http.build();
+
+    }
 }
