dca91186d1ec012b1415210b4d84528cd492849a
Zaid <pilarads06@gmail.com>
2024-05-09 12:31:51 +0200

    controller tienda 02

--- WorkSpaceMS/PilarAguilarTiendaOnline02/src/main/java/com/salesianostriana/dam/pilaraguilartiendaonline02/controller/ProductController.java
+++ WorkSpaceMS/PilarAguilarTiendaOnline02/src/main/java/com/salesianostriana/dam/pilaraguilartiendaonline02/controller/ProductController.java
@@ -3,6 +3,7 @@ package com.salesianostriana.dam.pilaraguilartiendaonline02.controller;
 
 import java.util.List;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -16,7 +17,7 @@ import com.salesianostriana.dam.pilaraguilartiendaonline02.service.ProductServic
 
 
 
-
+@Controller
 public class ProductController {
 
 	
