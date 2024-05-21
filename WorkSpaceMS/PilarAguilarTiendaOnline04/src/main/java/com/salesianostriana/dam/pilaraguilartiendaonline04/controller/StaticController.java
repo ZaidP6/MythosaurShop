c5f742da9d8d497dc1fef4355e37d0e868c96579
Zaid <pilarads06@gmail.com>
2024-05-21 11:51:15 +0200

    pequeñas correcciones en html

--- WorkSpaceMS/PilarAguilarTiendaOnline04/src/main/java/com/salesianostriana/dam/pilaraguilartiendaonline04/controller/StaticController.java
+++ WorkSpaceMS/PilarAguilarTiendaOnline04/src/main/java/com/salesianostriana/dam/pilaraguilartiendaonline04/controller/StaticController.java
@@ -1,60 +1,85 @@
 package com.salesianostriana.dam.pilaraguilartiendaonline04.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.salesianostriana.dam.pilaraguilartiendaonline04.model.Category;
+import com.salesianostriana.dam.pilaraguilartiendaonline04.service.CategoryService;
 
 @Controller
 public class StaticController {
+	
+	@Autowired
+	private CategoryService categoryService;
+	
+	public void llamarCategorias(Model model) {
+		List<Category> categorias = categoryService.findAll();
+		model.addAttribute("categorias", categorias);
+	}
 
 	@GetMapping("/contenidoContacto")
-	public String contenidoContacto() {
+	public String contenidoContacto(Model model) {
+		llamarCategorias(model);
 		return "contenidoContacto";
 	}
 	
 	@GetMapping("/trabajaConNosotros")
-	public String trabaja() {
+	public String trabaja(Model model) {
+		llamarCategorias(model);
 		return "trabajaConNosotros";
 	}
 	
 	@GetMapping("/quienesSomos")
-	public String quienesSomos() {
+	public String quienesSomos(Model model) {
+		llamarCategorias(model);
 		return "quienesSomos";
 	}
 	
 	@GetMapping("/politicaPrivacidad")
-	public String privacidad() {
+	public String privacidad(Model model) {
+		llamarCategorias(model);
 		return "politicaPrivacidad";
 	}
 	
 	@GetMapping("/cookies")
-	public String cookies() {
+	public String cookies(Model model) {
+		llamarCategorias(model);
 		return "cookies";
 	}
 	
 	//----------------------------------------------------------
 	
 	@GetMapping("/user/contenidoContacto")
-	public String contenidoContactoC() {
+	public String contenidoContactoC(Model model) {
+		llamarCategorias(model);
 		return "customer/contenidoContacto";
 	}
 	
 	@GetMapping("/user/trabajaConNosotros")
-	public String trabajaC() {
+	public String trabajaC(Model model) {
+		llamarCategorias(model);
 		return "customer/trabajaConNosotros";
 	}
 	
 	@GetMapping("/user/quienesSomos")
-	public String quienesSomosC() {
+	public String quienesSomosC(Model model) {
+		llamarCategorias(model);
 		return "customer/quienesSomos";
 	}
 	
 	@GetMapping("/user/politicaPrivacidad")
-	public String privacidadC() {
+	public String privacidadC(Model model) {
+		llamarCategorias(model);
 		return "customer/politicaPrivacidad";
 	}
 	
 	@GetMapping("/user/cookies")
-	public String cookiesC() {
+	public String cookiesC(Model model) {
+		llamarCategorias(model);
 		return "customer/cookies";
 	}
 	
@@ -62,27 +87,32 @@ public class StaticController {
 	//---------------------------------------------------------------
 	
 	@GetMapping("/admin/contenidoContacto")
-	public String contenidoContactoA() {
+	public String contenidoContactoA(Model model) {
+		llamarCategorias(model);
 		return "admin/contenidoContacto";
 	}
 	
 	@GetMapping("/admin/trabajaConNosotros")
-	public String trabajaA() {
+	public String trabajaA(Model model) {
+		llamarCategorias(model);
 		return "admin/trabajaConNosotros";
 	}
 	
 	@GetMapping("/admin/quienesSomos")
-	public String quienesSomosA() {
+	public String quienesSomosA(Model model) {
+		llamarCategorias(model);
 		return "admin/quienesSomos";
 	}
 	
 	@GetMapping("/admin/politicaPrivacidad")
-	public String privacidadA() {
+	public String privacidadA(Model model) {
+		llamarCategorias(model);
 		return "admin/politicaPrivacidad";
 	}
 	
 	@GetMapping("/admin/cookies")
-	public String cookiesA() {
+	public String cookiesA(Model model) {
+		llamarCategorias(model);
 		return "admin/cookies";
 	}
 	
