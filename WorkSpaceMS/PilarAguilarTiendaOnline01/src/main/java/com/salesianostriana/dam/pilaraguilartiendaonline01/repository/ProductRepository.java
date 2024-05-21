98e3bd22009a5bc859d959c756699fee114fa37b
Zaid <pilarads06@gmail.com>
2024-05-21 13:17:09 +0200

    Se borran proyectos anteriores para no dar confusion y se actualizan los recursos a consultar

--- WorkSpaceMS/PilarAguilarTiendaOnline01/src/main/java/com/salesianostriana/dam/pilaraguilartiendaonline01/repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.salesianostriana.dam.pilaraguilartiendaonline01.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.salesianostriana.dam.pilaraguilartiendaonline01.model.Product;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-
-}
