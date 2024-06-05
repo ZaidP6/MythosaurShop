060c49c1632767f69fd15e67946cb01a72b179ff
ZaidP6 <pilarads06@gmail.com>
2024-06-05 23:09:25 +0200

    nueva linea y obtener carrito en realidad...no se...

--- WorkSpaceMS/PilarAguilarTiendaOnline04/src/main/java/com/salesianostriana/dam/pilaraguilartiendaonline04/service/CartService.java
+++ WorkSpaceMS/PilarAguilarTiendaOnline04/src/main/java/com/salesianostriana/dam/pilaraguilartiendaonline04/service/CartService.java
@@ -1,34 +1,75 @@
 package com.salesianostriana.dam.pilaraguilartiendaonline04.service;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.salesianostriana.dam.pilaraguilartiendaonline04.model.Customer;
+import com.salesianostriana.dam.pilaraguilartiendaonline04.model.OrderLine;
 import com.salesianostriana.dam.pilaraguilartiendaonline04.model.OrderPedido;
 import com.salesianostriana.dam.pilaraguilartiendaonline04.model.Product;
+import com.salesianostriana.dam.pilaraguilartiendaonline04.repository.OrderRepository;
+import com.salesianostriana.dam.pilaraguilartiendaonline04.repository.ProductRepository;
 
 @Service
 public class CartService{
 
+	@Autowired
 	private OrderService orderService;
 	
+	@Autowired
+	private OrderRepository orderRepository;
+	
+	@Autowired
+	private ProductRepository productRepository;
+	
+					
+					//NUEVO CARRITO
 	
 	public OrderPedido newCart(Customer c) {
 		OrderPedido carrito = OrderPedido.builder().customer(c).orderFinished(false).build();
+		orderRepository.save(carrito);
 		return carrito;
 	}
 	
+					//CREAR Y AÑADIR LINEA A LA VENTA
+	
+	public void addOrderLine(Long orderId, Long productId, int quantity) {
+        Optional<OrderPedido> optionalOrder = orderService.findById(orderId);
+        Optional<Product> optionalProduct = productRepository.findById(productId);
+
+        if (optionalOrder.isPresent() && optionalProduct.isPresent()) {
+            OrderPedido order = optionalOrder.get(); //orElseThrow???
+            Product product = optionalProduct.get();
+
+            OrderLine orderLine = OrderLine.builder()
+                    .orderPedido(order)
+                    .product(product)
+                    .orderLineQuantity(quantity)
+                    .orderLinePrice(product.getProductPvP() * quantity)
+                    .build();
+
+            order.addOrderLine(orderLine);
+            orderService.actualizarVenta(order);
+        }
+    }
+	
+					//OBTENER CARRITO
 	
 	public OrderPedido getCart(Customer c) {
 		
-		return orderService.orderNotFinished(c);
+		return orderService.findByOpenOrder(c).get(); //.orElseGet(() -> newCart());
 		
 	}
 	
+					//AÑADIR PRODUCTO AL CARRITO
 	
 	public void addToCart(Customer c, Product p, int cantidad) {
 		
+		Optional<OrderPedido> optionalCart = orderService.findByOpenOrder(c);
+		OrderPedido cart = optionalCart.orElseGet(() -> newCart(c));
+		addOrderLine(cart.getOrderId(), p.getProductId(), cantidad);
 	}
-	
-	
 	
 }
--- WorkSpaceMS/PilarAguilarTiendaOnline04/src/main/java/com/salesianostriana/dam/pilaraguilartiendaonline04/service/OrderService.java
+++ WorkSpaceMS/PilarAguilarTiendaOnline04/src/main/java/com/salesianostriana/dam/pilaraguilartiendaonline04/service/OrderService.java
@@ -1,147 +1,137 @@
 package com.salesianostriana.dam.pilaraguilartiendaonline04.service;
 
-import java.time.LocalDateTime;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import com.salesianostriana.dam.pilaraguilartiendaonline04.model.Customer;
 import com.salesianostriana.dam.pilaraguilartiendaonline04.model.OrderLine;
 import com.salesianostriana.dam.pilaraguilartiendaonline04.model.OrderPedido;
 import com.salesianostriana.dam.pilaraguilartiendaonline04.model.Product;
 import com.salesianostriana.dam.pilaraguilartiendaonline04.repository.OrderRepository;
+import com.salesianostriana.dam.pilaraguilartiendaonline04.repository.ProductRepository;
 import com.salesianostriana.dam.pilaraguilartiendaonline04.service.base.BaseServiceImpl;
 
 @Service
 public class OrderService extends BaseServiceImpl<OrderPedido, Long, OrderRepository> {
 
 	@Autowired
-	private ProductService productService;
+	private ProductRepository productRepository;
 	
-	
-	public boolean orderNotFinished(Customer c) {
-		if() {
-			return false;
-		}
-		
-		return true;
+	public Optional<OrderPedido> findById(Long id) {
+		return this.repository.findById(id);
+	}
+
+	public List<OrderPedido> findAll() {
+		return this.repository.findAll();
+	}
+
+	public Optional<OrderPedido> findByOpenOrder(Customer c) {
+
+		return this.repository.findFirstByFinishedAndUser(false, c);
 	}
 	
 	
+	public OrderPedido actualizarVenta(OrderPedido order) {
+		return this.repository.save(order);
+	}
 	
+
 	/*
-	 * //listar las líneas del carrito
-	public List<OrderLine> listarLineasVenta() {
-		return Collections.unmodifiableList(olRepository.findAll());
-	}
+	 * //listar las líneas del carrito public List<OrderLine> listarLineasVenta() {
+	 * return Collections.unmodifiableList(olRepository.findAll()); }
 	 */
 
-	//método para añadir una línea al carrito y por tanto, actualizar dicha compra
+	// método para añadir una línea al carrito y por tanto, actualizar dicha compra
 	/*
 	 * public OrderLine addOrderLine(Long orderId, Long productId, int quantity) {
-		// Obtenemos venta por ID
-		OrderPedido orderPedido = orderRepository.findById(orderId)
-				.orElseThrow(() -> new IllegalArgumentException("Venta no encontrada"));
-
-		// Obtenemos producto por ID
-		Product product = productRepository.findById(productId)
-				.orElseThrow(() -> new IllegalArgumentException("Producto no encontrado"));
-
-		// Se crea nueva linea
-		OrderLine orderLine = OrderLine.builder()
-				.orderPedido(orderPedido)
-				.product(product)
-				.orderLineQuantity(quantity)
-				.orderLinePrice(product.getProductPvP() * quantity)
-				.build();
-
-		// se añade linea a venta
-        orderPedido.addOrderLine(orderLine);
-
-        // se guarda
-        orderRepository.save(orderLine);
-
-        // Actualiza y guarda la venta
-        orderRepository.save(orderPedido);
-
-        return orderLine;
-	}
+	 * // Obtenemos venta por ID OrderPedido orderPedido =
+	 * orderRepository.findById(orderId) .orElseThrow(() -> new
+	 * IllegalArgumentException("Venta no encontrada"));
+	 * 
+	 * // Obtenemos producto por ID Product product =
+	 * productRepository.findById(productId) .orElseThrow(() -> new
+	 * IllegalArgumentException("Producto no encontrado"));
+	 * 
+	 * // Se crea nueva linea OrderLine orderLine = OrderLine.builder()
+	 * .orderPedido(orderPedido) .product(product) .orderLineQuantity(quantity)
+	 * .orderLinePrice(product.getProductPvP() * quantity) .build();
+	 * 
+	 * // se añade linea a venta orderPedido.addOrderLine(orderLine);
+	 * 
+	 * // se guarda orderRepository.save(orderLine);
+	 * 
+	 * // Actualiza y guarda la venta orderRepository.save(orderPedido);
+	 * 
+	 * return orderLine; }
 	 */
-	
-	//método para borrar una linea del carrito
+
+	// método para borrar una linea del carrito
 	/*
-	 * public void deleteOrderLine(Long orderLineId) {
-        OrderLine orderLine = olRepository.findById(orderLineId)
-                .orElseThrow(() -> new IllegalArgumentException("Línea de venta no encontrada"));
-
-        OrderPedido orderPedido = orderLine.getOrderPedido();
-        orderPedido.removeOrderLine(orderLine);
-
-        olRepository.delete(orderLine);
-        orderRepository.save(orderPedido);
-    }
+	 * public void deleteOrderLine(Long orderLineId) { OrderLine orderLine =
+	 * olRepository.findById(orderLineId) .orElseThrow(() -> new
+	 * IllegalArgumentException("Línea de venta no encontrada"));
+	 * 
+	 * OrderPedido orderPedido = orderLine.getOrderPedido();
+	 * orderPedido.removeOrderLine(orderLine);
+	 * 
+	 * olRepository.delete(orderLine); orderRepository.save(orderPedido); }
 	 */
-	
-	//Avtualizar cantidad de un producto
-	
-		/*
-		 * public OrderLine actualizarOrderLine(Long orderLineId, Long productId, int quantity) {
-	        OrderLine orderLine;
 
-	        if (orderLineId != null) {
-	            // Si existe el ID, actualizamos la línea de pedido
-	            orderLine = olRepository.findById(orderLineId)
-	                    .orElseThrow(() -> new IllegalArgumentException("Línea de venta no encontrada"));
+	// Avtualizar cantidad de un producto
 
-	            // Actualizamos la cantidad y el precio
-	            orderLine.setOrderLineQuantity(quantity);
-	            orderLine.setOrderLinePrice(orderLine.getProduct().getProductPvP() * quantity);
-	        } else {
-	            // Si no existe el ID, creamos una nueva línea de pedido vacía
-	            orderLine = new OrderLine();
-	        }
+	/*
+	 * public OrderLine actualizarOrderLine(Long orderLineId, Long productId, int
+	 * quantity) { OrderLine orderLine;
+	 * 
+	 * if (orderLineId != null) { // Si existe el ID, actualizamos la línea de
+	 * pedido orderLine = olRepository.findById(orderLineId) .orElseThrow(() -> new
+	 * IllegalArgumentException("Línea de venta no encontrada"));
+	 * 
+	 * // Actualizamos la cantidad y el precio
+	 * orderLine.setOrderLineQuantity(quantity);
+	 * orderLine.setOrderLinePrice(orderLine.getProduct().getProductPvP() *
+	 * quantity); } else { // Si no existe el ID, creamos una nueva línea de pedido
+	 * vacía orderLine = new OrderLine(); }
+	 * 
+	 * return olRepository.save(orderLine); }
+	 */
 
-	        return olRepository.save(orderLine);
-	    }
-		 */
-		
-		//método para calcular el total completo
-		/*
-		 * public double calcularTotal() { //Nuestro map con producto y cantidad
-		 * Map<Product, Integer> carrito = this.productoKey; double total = 0.0;
-		 * 
-		 * //otro map para albergar cada linea completa for (Map.Entry<Product, Integer>
-		 * entry : carrito.entrySet()) { Product producto = entry.getKey(); int cantidad
-		 * = entry.getValue(); total += producto.getProductPvP() * cantidad; } return
-		 * total; }
-		 */
+	// método para calcular el total completo
+	/*
+	 * public double calcularTotal() { //Nuestro map con producto y cantidad
+	 * Map<Product, Integer> carrito = this.productoKey; double total = 0.0;
+	 * 
+	 * //otro map para albergar cada linea completa for (Map.Entry<Product, Integer>
+	 * entry : carrito.entrySet()) { Product producto = entry.getKey(); int cantidad
+	 * = entry.getValue(); total += producto.getProductPvP() * cantidad; } return
+	 * total; }
+	 */
 
-		// Agregar producto al carrito
-		/*
-		 * public void agregarProducto(Product producto, int cantidad) {
-		 * 
-		 * if (productoKey.containsKey(producto)) { if
-		 * (productoKey.containsKey(producto)) { if (productoKey.get(producto) > 1)
-		 * productoKey.replace(producto, productoKey.get(producto) - 1); else if
-		 * (productoKey.get(producto) == 1) { productoKey.remove(producto); } } }
-		 * 
-		 * }
-		 */
+	// Agregar producto al carrito
+	/*
+	 * public void agregarProducto(Product producto, int cantidad) {
+	 * 
+	 * if (productoKey.containsKey(producto)) { if
+	 * (productoKey.containsKey(producto)) { if (productoKey.get(producto) > 1)
+	 * productoKey.replace(producto, productoKey.get(producto) - 1); else if
+	 * (productoKey.get(producto) == 1) { productoKey.remove(producto); } } }
+	 * 
+	 * }
+	 */
 
-		/*
-		 * // Eliminar producto del carrito public void eliminarProducto(Product
-		 * producto) { this.productoKey.remove(producto); }
-		 * 
-		 * // Obtener la cantidad de un producto específico public int
-		 * obtenerCantidad(Product producto) { return
-		 * this.productoKey.getOrDefault(producto, 0); }
-		 * 
-		 * // Ver todos los productos en el carrito public Map<Product, Integer>
-		 * verProductosCarrito() { return Collections.unmodifiableMap(this.productoKey);
-		 * }
-		 */
+	/*
+	 * // Eliminar producto del carrito public void eliminarProducto(Product
+	 * producto) { this.productoKey.remove(producto); }
+	 * 
+	 * // Obtener la cantidad de un producto específico public int
+	 * obtenerCantidad(Product producto) { return
+	 * this.productoKey.getOrDefault(producto, 0); }
+	 * 
+	 * // Ver todos los productos en el carrito public Map<Product, Integer>
+	 * verProductosCarrito() { return Collections.unmodifiableMap(this.productoKey);
+	 * }
+	 */
 
 }
