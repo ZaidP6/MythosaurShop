3c26f3cdfa36dab8c42285aa0a0d8ae9d5978e1b
ZaidP6 <pilarads06@gmail.com>
2024-06-19 21:15:27 +0200

    formulario signIn validacion

--- WorkSpaceMS/PilarAguilarTiendaOnline04/src/main/resources/static/js/validacionSignIn.js
+++ WorkSpaceMS/PilarAguilarTiendaOnline04/src/main/resources/static/js/validacionSignIn.js
@@ -26,3 +26,38 @@ form.addEventListener('submit', function(event) {
 
 passwordInput.addEventListener('input', validarContraseña);
 passwordConfInput.addEventListener('input', validarContraseña);
+
+
+let birthInput = document.getElementById('inputBirthDay');
+    let ageError = document.getElementById('ageError');
+
+    function validateAge() {
+        let birthDate = new Date(birthInput.value);
+        let today = new Date();
+        let age = today.getFullYear() - birthDate.getFullYear();
+        let monthDifference = today.getMonth() - birthDate.getMonth();
+
+        if (monthDifference < 0 || (monthDifference === 0 && today.getDate() < birthDate.getDate())) {
+            age--;
+        }
+
+        if (age < 18) {
+            ageError.textContent = 'Debes tener al menos 18 años';
+            birthInput.classList.add('is-invalid');
+            return false;
+        } else {
+            ageError.textContent = '';
+            birthInput.classList.remove('is-invalid');
+            birthInput.classList.add('is-valid');
+            return true;
+        }
+    }
+
+    birthInput.addEventListener('change', validateAge);
+
+    let formBirth = document.querySelector('form');
+    formBirth.addEventListener('submit', function (event) {
+        if (!validateAge()) {
+            event.preventDefault();
+        }
+    });
